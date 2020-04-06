6110d7047b557265416d9279e09a8b4b013aba8c
murat.gundes <murat.gundes@hotmail.com>
2020-04-06 18:18:20 +0200

    Product repository added for dummy products.

--- TariffComparison/TariffComparison.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TariffComparison.Repository
-{
-    public class Class1
-    {
-    }
-}
---
+++ TariffComparison/TariffComparison.Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using TariffComparison.Contract.Interfaces;
+using TariffComparison.Domain;
+
+namespace TariffComparison.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public List<ITariff> GetProducts()
+        {
+            return new List<ITariff>
+            {
+                new BasicElectricityTariff(),
+                new PackagedTariff()
+            };
+        }
+    }
+}
