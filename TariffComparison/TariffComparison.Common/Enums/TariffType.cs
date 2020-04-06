302cc78ccc2975ecc4d1db55c4901355b24c10dc
murat.gundes <murat.gundes@hotmail.com>
2020-04-06 19:32:36 +0200

    tariff type enum description text added.

--- TariffComparison/TariffComparison.Common/Enums/TariffType.cs
+++ TariffComparison/TariffComparison.Common/Enums/TariffType.cs
@@ -1,8 +1,16 @@
-﻿namespace TariffComparison.Common.Enums
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TariffComparison.Common.Enums
 {
     public enum TariffType
     {
-        Basic,
-        Packaged
+        [Description("BasicElectricityTariff")]
+        [Display(Name = "Basic")]
+        Basic = 1,
+
+        [Description("PackagedTariff")]
+        [Display(Name = "Packaged")]
+        Packaged = 2
     }
 }
